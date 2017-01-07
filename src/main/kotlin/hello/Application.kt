3445c93cb6dd025e4ab05442e4adb8dd9db1c1fd
robmoore <rmoore@epatientfinder.com>
2017-01-06 18:04:14 -0600

    - Adding retryable to the mix.

--- src/main/kotlin/hello/Application.kt
+++ src/main/kotlin/hello/Application.kt
@@ -2,9 +2,10 @@ package hello
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-
+import org.springframework.retry.annotation.EnableRetry
 
 @SpringBootApplication
+@EnableRetry
 class Application
 
 fun main(args: Array<String>) {
--- src/main/kotlin/hello/Quote.kt
+++ src/main/kotlin/hello/Quote.kt
@@ -1,8 +1,12 @@
 package hello
 
 import mu.KLogging
+import org.springframework.retry.annotation.Retryable
+import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.HttpServerErrorException
+import org.springframework.web.client.ResourceAccessException
 import org.springframework.web.client.RestTemplate
 
 data class Value(val id: Long, val quote: String)
@@ -10,11 +14,22 @@ data class Value(val id: Long, val quote: String)
 data class Quote(val type: String, val value: Value)
 
 @RestController
-class QuoteController {
+class QuoteController(val quoteService: QuoteService) {
     companion object : KLogging()
 
     @GetMapping("/quote")
-    fun fetchQuote(restTemplate: RestTemplate): Quote {
+    fun quote(): Quote {
+        logger.debug { "Requesting quote" }
+        return quoteService.fetchQuote()
+    }
+}
+
+@Service
+@Retryable(include = arrayOf(HttpServerErrorException::class, ResourceAccessException::class))
+class QuoteService(val restTemplate: RestTemplate = RestTemplate()) {
+    companion object : KLogging()
+
+    fun fetchQuote(): Quote {
         val quote = restTemplate
                 .getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote::class.java)
         logger.debug { "Quote: $quote" }
